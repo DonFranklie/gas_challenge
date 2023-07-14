170b50ee7bcf85351a1be0b68a459cc4ac3a90fb
Don Franklie <donfranklie@gmail.com>
2023-07-14 09:56:49 +0300

    completed bounty

--- contracts/gasChallenge.sol
+++ contracts/gasChallenge.sol
@@ -2,11 +2,8 @@
 pragma solidity ^0.8.0;
 
 contract gasChallenge {
-    //Implement Fixed-Size Array Technique Here
-    uint[] numbers = [1,2,3,4,5,6,7,8,9,10];
+    uint[10] numbers = [1,2,3,4,5,6,7,8,9,10]; // Using fixed-size array
     
-    //Function to check for sum of array
-    //No changes required in this function
     function getSumOfArray() public view returns (uint256) {
         uint sum = 0;
         for (uint i = 0; i < numbers.length; i++) {
@@ -21,9 +18,11 @@ contract gasChallenge {
         }
     }
     
-    //Implement Remaining Gas Optimization Techniques Here
-    //Sum of elements in the numbers array should equal 0
     function optimizedFunction() public {
-      
+        unchecked { // Using unchecked block to skip overflow checks
+            for (uint i = 0; i < numbers.length; i++) {
+                numbers[i] = 0;
+            }
+        }
     }
- }
+}
